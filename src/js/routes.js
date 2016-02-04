86d166515471fd046de65e4576c2ba3bd960b12c
Ryan Urabe <rurabe@gmail.com>
2016-02-03 16:27:53 -1000

    authorizations aiya

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,4 +1,5 @@
 'use strict';
+var express = require('express');
 
 const IndexController = require('./controllers/index_controller');
 const AddressesController = require('./controllers/addresses_controller');
@@ -11,6 +12,14 @@ const Passport = require('./config/passport');
 
 const bp = require('body-parser');
 
+const _authenticate = function(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  } else {
+    res.status(401).json({error: 'Unauthorized'});
+  }
+}
+
 const Routes = {
   init: function(app){
     app.use(bp.json({ type: 'application/json' }))
@@ -20,31 +29,44 @@ const Routes = {
     //   /logout
     Passport.routes(app);
 
-    app.get("/datacenters/:datacenter_id/servers/:id",IndexController.index);
-    app.get("/datacenters/:datacenter_id/ranges/:id",IndexController.index);
-    app.get("/datacenters/:id/",IndexController.index)
     app.get("/",IndexController.index);
 
 
-    app.get('/api/addresses',AddressesController.index);
-    app.put('/api/addresses/:id',AddressesController.update);
+    let pages = express.Router();
+    pages.use(_authenticate);
 
-    app.get('/api/datacenters',DatacentersController.index);
-    app.post('/api/datacenters',DatacentersController.create);
-    app.put('/api/datacenters/:id',DatacentersController.update);
-    app.delete('/api/datacenters/:id',DatacentersController.destroy);
-    
-    app.get('/api/ranges',RangesController.index);
-    app.post('/api/ranges',RangesController.create);
-    app.put('/api/ranges/:id',RangesController.update);
-    app.delete('/api/ranges/:id',RangesController.destroy);
-    
-    app.get('/api/servers',ServersController.index);
-    app.post('/api/servers',ServersController.create);
-    app.put('/api/servers/:id',ServersController.update);
-    app.delete('/api/servers/:id',ServersController.destroy);
+    pages.get("/datacenters/:datacenter_id/servers/:id",IndexController.index);
+    pages.get("/datacenters/:datacenter_id/ranges/:id",IndexController.index);
+    pages.get("/datacenters/:id/",IndexController.index)
 
-    app.post('/api/datacenters/:datacenter_id/server_assignments',ServerAssignmentsController.create);
+    app.use('/',pages);
+
+
+
+    let api = express.Router();
+    api.use(_authenticate);
+
+    api.get('/addresses',AddressesController.index);
+    api.put('/addresses/:id',AddressesController.update);
+
+    api.get('/datacenters',DatacentersController.index);
+    api.post('/datacenters',DatacentersController.create);
+    api.put('/datacenters/:id',DatacentersController.update);
+    api.delete('/datacenters/:id',DatacentersController.destroy);
+    
+    api.get('/ranges',RangesController.index);
+    api.post('/ranges',RangesController.create);
+    api.put('/ranges/:id',RangesController.update);
+    api.delete('/ranges/:id',RangesController.destroy);
+    
+    api.get('/servers',ServersController.index);
+    api.post('/servers',ServersController.create);
+    api.put('/servers/:id',ServersController.update);
+    api.delete('/servers/:id',ServersController.destroy);
+
+    api.post('/datacenters/:datacenter_id/server_assignments',ServerAssignmentsController.create);
+
+    app.use('/api',api);
   }
 };
 
